0e6ff7349c53d1fc3b801226e973678e5d03ff3f
jgarciam12 <javiergrs92@gmail.com>
2026-02-22 20:16:45 -0500

    fix: ajustando el reescalamiento de las imagenes antes de predecir

--- src/inference/predictor.py
+++ src/inference/predictor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import json
 
 class Predictor:
     def __init__(self, model_path: str, img_size: int = 224):
@@ -9,7 +10,8 @@ class Predictor:
 
     def predict(self, image: np.ndarray) -> np.ndarray:
         image = image.resize((self.img_size, self.img_size))
-        x = np.expand_dims(np.array(image), axis=0)
+        x = np.array(image, dtype=np.float32) / 255.0
+        x = np.expand_dims(x, axis=0)
 
         pred = self.model.predict(x)
         idx = int(np.argmax(pred))
