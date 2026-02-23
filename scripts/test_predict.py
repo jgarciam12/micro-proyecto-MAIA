1c54262a476763878d44dd77c41d0c2fe9e3af14
jgarciam12 <javiergrs92@gmail.com>
2026-02-22 20:19:32 -0500

    fix: ajustando el reescalamiento de las imagenes antes de predecir

--- scripts/test_predict.py
+++ scripts/test_predict.py
@@ -1,18 +1,24 @@
 from PIL import Image
 from src.inference.predictor import Predictor
+import os
 
 def main():
     predictor = Predictor(
         model_path = "artifacts/models/scoliosis_model.keras",
         img_size = 224
     )
+    PATH = "data/data-xray/224/224/Scol"
+    imagenes = os.listdir(PATH)
 
-    img = Image.open("data/data-xray/224/224/Scol/N15, Rt TAIS, F, 14 yrs.jpg")#)Normal/N1,N,40,M_1_0.jpg")
+    for imagename in imagenes:
+        img = Image.open(os.path.join(PATH, imagename))
 
-    result = predictor.predict(img)
+    #img = Image.open("/N15, Rt TAIS, F, 14 yrs.jpg")#)Normal/N1,N,40,M_1_0.jpg")
 
-    print("\n==== RESULTADO ====")
-    print(result)
+        result = predictor.predict(img)
+
+        print("\n==== RESULTADO ====")
+        print(result)
 
 if __name__ == "__main__":
     main()
